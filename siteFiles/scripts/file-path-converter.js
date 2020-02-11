8ea8784dfcfbd3392b4df5f44a47ba8b8490a879
Dan <daniel.nehl.it@gmail.com>
2020-02-12 09:33:05 +1000

    Fixes and changes a plenty

--- siteFiles/scripts/file-path-converter.js
+++ siteFiles/scripts/file-path-converter.js
@@ -1,10 +1,15 @@
-// VARS
+
 // var exampleWin = "C:\Dan\Arc\Teach\Other\APCSSci\2015\Resoucres\Yr7\"
 // escaped/encoded using encodeURIComponent() below 
 // path = "C%3A%5CDan%5CArc%5CTeach%5COther%5CAPCSSci%5C2015%5CResoucres%5CYr7%5C"
 // "/" = %2F 
 // var exampleLinux = "/Dan/Arc/Teach/Other/APCSSci/2015/Resoucres/Yr7/" 
 
+// ------------ VARS ----------------
+const fwdSlash = "%2F"; 
+const backSlash = "%5C"; 
+
+/*
 // Copy input from user
 function copyInput() {
     var copyText = document.querySelector("#pathInput");
@@ -22,31 +27,30 @@ function onclick(event) {
   }
 
 // https://www.the-art-of-web.com/javascript/escape/
-
+*/
 
 // Windows --> Linux file path
 function convertPathWindows(path) {
-    let newPath = "%2F "; // first forward slash
-    // Add respective drive mount to start of file path 
-    // INTERNAL DRIVE - eg linux and windows equivalent: /  and  C:\
-    if (i<=1 && path.charAt(0) == "C") {
-        newChar = "";
-    }
-    else { // EXTERNAL DRIVE -eg linux and windows equivalent: /mnt/d/  and  D:\  
-        // add mount    NB: "/" = %2F 
-        newPath += "mnt%2F"; 
+    let newPath = fwdSlash; 
+    // add mount if needed (otherwise just forward slash on linux)
+    if (path.charAt(0) != "C") {
+        // EXTERNAL DRIVE - eg linux & windows equivalent: /mnt/d/  and  D:\ 
+        newPath += "mnt" + fwdSlash; 
         // change case of drive letter and add
         newPath += currenChar.toLowerCase(); 
-    }
-    for (i=2;length(path);i++) {
-        // otherwise keep char same
-        if (currentChar != "%" && path.charAt(i+1) !="5" && path.charAt(i+2) != "C") {
+    } 
+    for (i=2;i<path.length;i++) {
+        currentChar = path.charAt(i);
+        // if not backslash char
+        if ((currentChar != "%" && path.charAt(i+1) !="5" && path.charAt(i+2) != "C") || 
+        (currentChar != "5" && path.charAt(i+1) !="C" && path.charAt(i-1) != "%")     ||
+        (currentChar != "C" && path.charAt(i-2) !="%" && path.charAt(i-1) != "5")   ) {
             // normal char - simply add
-            newChar = currentChar;
+            newPath += currentChar;
         }
-        // Add the rest of the chars 
-        else  {  // current char is \
-            newChar = '%2F'; // change to /
+        // Add slash only once for each of "%5C"
+        else if (currentChar == "%") {  
+            newPath += fwdSlash; 
         }
     }
     return newPath; 
@@ -56,9 +60,10 @@ function convertPathWindows(path) {
 // Linux --> Windows file path
 function convertPathLinux(path) {
     let newPath = "";
+    currentChar = "placeholder"
     // swap the slashes (back/forward) 
-    if (currentChar =="/") {
-        newPath += '\\';
+    if (currentChar =="placeholder") {
+        newPath += 'placeholder';
     }
     // otherwise keep char same
     else {
@@ -77,7 +82,7 @@ function convertPaths(path) {
     }
     // Linux --> Windows file path
     else {
-        newPath = convertPathLinux(path)
+        newPath = convertPathLinux(path);
     }
     return newPath; 
 }
@@ -86,6 +91,7 @@ function convertPaths(path) {
 // TEMP 
 console.log(decodeURIComponent(convertPaths(path)));
 // 
+
 
 
 
